c1823e3a78e7ce32af642b59290b4347af5249d6
Ayaka <dungbui1806h@gmail.com>
2026-09-25 16:46:34 +0700

    Revert "."
    This reverts commit d43e3ff5f6c529dd3c485a67c345460a560bdb11.

--- src/main/java/Controller/GameManager.java
+++ src/main/java/Controller/GameManager.java
@@ -1,27 +1,13 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Controller;
 
-
+/**
+ *
+ * @author ADMIN
+ */
 public class GameManager {
-    private LevelLoader levelLoader;
-    private int[][] currentMap;
-
-    public GameManager() {
-        levelLoader = new LevelLoader();
-    }
-
-    public void startGame() {
-        System.out.println("Đang khởi động Candy Crush...");
-        // Gọi hàm loadMap với bản đồ 8x8
-        currentMap = levelLoader.loadMap("map.txt", 8, 8);
-
-        // In mảng 2 chiều ra màn hình Console để test
-        System.out.println("Đã nạp bản đồ thành công:");
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(currentMap[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+    
 }
--- src/main/java/Main/App.java
+++ src/main/java/Main/App.java
@@ -1,10 +1,13 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Main;
-import Controller.GameManager;
 
+/**
+ *
+ * @author ADMIN
+ */
 public class App {
-    public static void main(String[] args) {
-        GameManager game = new GameManager();
-        game.startGame();
-    }
+    
 }
